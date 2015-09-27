f10579c1c8f3bb7e46e42ef0dd7155c0b2030c54
Oleg Krivosheev <Oleg_Krivosheev@yahoo.com>
2015-09-27 10:46:52 -0400

    cachematrix.R
    Test added, more comments

--- main.R
+++ main.R
@@ -1,3 +1,5 @@
+## Test matrix cache code
+
 source("cachematrix.R")
 
 # build 3x3 test matrix
@@ -6,7 +8,10 @@ mtx <- matrix( c(1.0,2.0,3.0,2.0,1.0,2.0,3.0,2.0,1.0), nrow=3, ncol=3, byrow=TRU
 # cache init
 cm = makeCacheMatrix(mtx)
 
+# computed inverse
 inv <- cacheSolve(cm)
+
+print("Using computed inverse")
 
 # check, shall produce unit 3x3 matrix
 print( round( inv %*% cm$get(), 7 ) )
@@ -14,3 +19,13 @@ print( round( inv %*% cm$get(), 7 ) )
 # check, shall produce unit 3x3 matrix
 print( round( cm$get() %*% inv, 7 ) )
 
+# cached inverse
+inv <- cacheSolve(cm)
+
+print("Using cached inverse")
+
+# check, shall produce unit 3x3 matrix
+print( round( inv %*% cm$get(), 7 ) )
+
+# check, shall produce unit 3x3 matrix
+print( round( cm$get() %*% inv, 7 ) )
